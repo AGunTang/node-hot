938323ffee7c3ee81b1a1e7ce4f9850fa9b6ad92
SmallHorseBrute <1778636186@qq.com>
2019-04-01 21:40:18 +0800

    Update orders.vue

--- src/components/orders.vue
+++ src/components/orders.vue
@@ -8,7 +8,7 @@
       <el-table-column prop="username" label="订单编号" width="180"></el-table-column>
       <el-table-column prop="email" label="订单价格" width="180"></el-table-column>
       <el-table-column prop="mobile" label="是否付款" width="180"></el-table-column>
-      <el-table-column prop="mg_state" label="是否发货" width="180"></el-table-column>
+      <el-table-column prop="mg_state" label="是否发货" width="180"> </el-table-column>
       <el-table-column prop="mg_state" label="下单时间" width="180"></el-table-column>
       <el-table-column prop="mobile" label="操作" width="180">
         <template slot-scope="scope">
